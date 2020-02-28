6e07be15094ce5657e7244ff61e9d6217908a835
Heng Ma <maheng1991@hotmail.com>
2020-02-28 17:17:09 -0600

    changed hardcoded dir names to var in bash script

--- predict_structure.sh
+++ predict_structure.sh
@@ -1,5 +1,9 @@
-bash ../data_processing/jackhmmer.sh 1UBQ.fa ../../dbs/proteinnet12 
-python ../data_processing/convert_to_proteinnet.py 1UBQ.fa
-python ../data_processing/convert_to_tfrecord.py 1UBQ.fa.proteinnet 1UBQ.fa.tfrecord 42 
+rgn_path=../rgn 
+dbs_path=../dbs/
+rgn_model_path=./RGN12/
+
+bash $rgn_path/data_processing/jackhmmer.sh 1UBQ.fa $dbs_path/proteinnet12 
+python $rgn_path/data_processing/convert_to_proteinnet.py 1UBQ.fa
+python $rgn_path/data_processing/convert_to_tfrecord.py 1UBQ.fa.proteinnet 1UBQ.fa.tfrecord 42 
 cp 1UBQ.fa.tfrecord RGN12/data/ProteinNet12Thinning90/testing/
-python ../model/protling.py RGN12/runs/CASP12/ProteinNet12Thinning90/configuration -d ./RGN12/ -p -e weighted_testing -g0 
+python $rgn_path/model/protling.py $rgn_model_path/runs/CASP12/ProteinNet12Thinning90/configuration -d $rgn_model_path -p -e weighted_testing -g0 
